a6c5e5818149e67f7aa89c969a2be5244f997c94
Murilo Luiz Jaboinski <143234115+muriloLuix@users.noreply.github.com>
2024-04-10 14:56:03 -0300

    Javascript para verificação

---
+++ src/js/verification.js
@@ -0,0 +1,18 @@
+const firstName = document.getElementById('firstName').value;
+const secondName = document.getElementById('secondName');
+const email = document.getElementById('email');
+const tel = document.getElementById('telefone');
+const cpf = document.getElementById('cpf');
+const msg = document.getElementsByClassName('modal-dialog');
+const btn = document.getElementById('btnForm')
+
+
+btn = document.addEventListener('click', function (){
+    if (firstName.length === 0){
+        alert(`Por favor, preencha os campos necessários!`)
+    } else{
+        return
+    }
+} )
+
+
