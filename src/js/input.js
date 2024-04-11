e6bc524f5a0c979c95f50acb639ebc6dda4479e4
Murilo Luiz Jaboinski <muriloluiz654@gmail.com>
2024-04-11 00:27:38 -0300

    Testando responsividade e javascript

--- src/js/input.js
+++ src/js/input.js
@@ -1,4 +1,3 @@
-
 $(document).ready(function () {
-    $('#date-input').mask('00/00/0000');
+    $('#date-input').inputmask('99/99/9999');
 });
--- src/js/verification.js
+++ src/js/verification.js
@@ -1,16 +1,43 @@
-// const firstName = document.getElementById('firstName').value;
-// const secondName = document.getElementById('secondName');
-// const email = document.getElementById('email');
-// const tel = document.getElementById('telefone');
-// const cpf = document.getElementById('cpf');
-// const msg = document.getElementsByClassName('modal-dialog');
-// const btn = document.getElementById('btnForm')
+// Aqui, busquei por todos os elementos que possuam input
+const inputFields = document.querySelectorAll("input");
 
+// field é um ouvinte, que a cada modificação adicionada no campo input, a borda vermelha é removida
+inputFields.forEach((field) => {
+  field.addEventListener("change", function () {
+    this.classList.remove("input-invalid");
+  });
+});
 
-// btn = document.addEventListener('click', function (){
-//     if (firstName.length === 0){
-//         alert(`Por favor, preencha os campos necessários!`)
-//     } else{
-//         return
-//     }
-// } )
+document.getElementById("btnForm").addEventListener("click", function () {
+  const firstName = document.getElementById("firstName");
+  const secondName = document.getElementById("secondName");
+  const email = document.getElementById("email");
+  const tel = document.getElementById("telefone");
+  const cpf = document.getElementById("cpf");
+  const servicos = document.getElementById("servicos");
+
+  const requiredFields = [firstName, secondName, email, tel, cpf, servicos];
+
+  requiredFields.forEach((field) => {
+    if (field.value.trim() === "") {
+      // Está verificando se o valor do input está vazio
+      field.classList.add("input-invalid");
+    } else {
+      field.classList.remove("input-invalid");
+    }
+  });
+
+  if (
+    firstName.value.trim() === "" ||
+    secondName.value.trim() === "" ||
+    email.value.trim() === "" ||
+    tel.value.trim() === "" ||
+    cpf.value.trim() === "" ||
+    servicos.value.trim()
+  ) {
+    alert(`Por favor, preencha todos os campos obrigatórios!`);
+    return;
+  }
+
+  $("#exampleModal").modal("show");
+});
