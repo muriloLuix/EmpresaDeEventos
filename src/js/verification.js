50b24b572aa2eb0dfffc1443444dc19212f11612
Murilo Luiz Jaboinski <muriloluiz654@gmail.com>
2024-04-11 01:14:36 -0300

    Responsividade e Impressão
    Adicionando versão de impressão e responsividade no form.hmtl

--- src/js/verification.js
+++ src/js/verification.js
@@ -1,43 +1,49 @@
-// Aqui, busquei por todos os elementos que possuam input
-const inputFields = document.querySelectorAll("input");
+document.addEventListener("DOMContentLoaded", function () {
+  // Para executar apenas quando a página toda for carregada
+  const inputFields = document.querySelectorAll("input");
 
-// field é um ouvinte, que a cada modificação adicionada no campo input, a borda vermelha é removida
-inputFields.forEach((field) => {
-  field.addEventListener("change", function () {
-    this.classList.remove("input-invalid");
+  inputFields.forEach((field) => {
+    // Criado um parametro que será recebido
+    field.addEventListener("change", function () {
+      this.classList.remove("input-invalid");
+    });
   });
-});
 
-document.getElementById("btnForm").addEventListener("click", function () {
-  const firstName = document.getElementById("firstName");
-  const secondName = document.getElementById("secondName");
-  const email = document.getElementById("email");
-  const tel = document.getElementById("telefone");
-  const cpf = document.getElementById("cpf");
-  const servicos = document.getElementById("servicos");
+  document.getElementById("btnForm").addEventListener("click", function () {
+    const firstName = document.getElementById("firstName");
+    const secondName = document.getElementById("secondName");
+    const email = document.getElementById("email");
+    const tel = document.getElementById("telefone");
+    const cpf = document.getElementById("cpf");
+    const servicos = document.getElementById("servicos");
 
-  const requiredFields = [firstName, secondName, email, tel, cpf, servicos];
+    const requiredFields = [firstName, secondName, email, tel, cpf, servicos];
 
-  requiredFields.forEach((field) => {
-    if (field.value.trim() === "") {
-      // Está verificando se o valor do input está vazio
-      field.classList.add("input-invalid");
-    } else {
-      field.classList.remove("input-invalid");
+    requiredFields.forEach((field) => {
+      // Está verificando se os inputs possuem algum valor em seu value
+      if (field.value.trim() === "") {
+        field.classList.add("input-invalid");
+      } else {
+        field.classList.remove("input-invalid");
+      }
+    });
+
+    if (
+      firstName.value.trim() === "" ||
+      secondName.value.trim() === "" ||
+      email.value.trim() === "" ||
+      tel.value.trim() === "" ||
+      cpf.value.trim() === "" ||
+      servicos.value.trim() === ""
+    ) {
+      alert(`Por favor, preencha todos os campos obrigatórios!`);
+      return;
     }
+
+    $("#exampleModal").modal("show");
+
+    inputFields.forEach((field) => {
+      field.value = "";
+    });
   });
-
-  if (
-    firstName.value.trim() === "" ||
-    secondName.value.trim() === "" ||
-    email.value.trim() === "" ||
-    tel.value.trim() === "" ||
-    cpf.value.trim() === "" ||
-    servicos.value.trim()
-  ) {
-    alert(`Por favor, preencha todos os campos obrigatórios!`);
-    return;
-  }
-
-  $("#exampleModal").modal("show");
 });
