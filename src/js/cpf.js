da28131cf9b29f9e5a57436172d698a4ea7ef963
Murilo Luiz Jaboinski <143234115+muriloLuix@users.noreply.github.com>
2024-04-15 12:26:11 -0300

    Ajustando comentários

--- src/js/cpf.js
+++ src/js/cpf.js
@@ -2,6 +2,7 @@ function formatarCPF(campo) {
   let cpf = campo.value.replace(/\D/g, "");
 
   // Adiciona a formatação
+  // "$1.$2" são as células, ou seja, após {3} campos, coloque um .
   cpf = cpf.replace(/(\d{3})(\d)/, "$1.$2");
   cpf = cpf.replace(/(\d{3})(\d)/, "$1.$2");
   cpf = cpf.replace(/(\d{3})(\d{1,2})$/, "$1-$2");
